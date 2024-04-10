8f187742f5a4a1bbdbbd5dc33832161fc00caca5
Cocostee <clementld76@gmail.com>
2024-04-10 21:55:28 +0200

    Initial Commit

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Users;
+use App\Form\UsersAddType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class UsersController extends AbstractController
+{
+
+    #[Route('/users/index', name: 'users')]
+    public function index(): Response
+    {   
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+        return $this->render('users/index.html.twig', [
+            'controller_name' => 'UsersController',
+        ]);
+    }   
+    
+    #[Route('/users/all', name: 'users_all')]
+    public function allUsers(EntityManagerInterface $em)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $users = $em->getRepository(Users::class)->findAll();
+        return $this->render('users/all.html.twig', ['users' => $users]);
+    }
+}
